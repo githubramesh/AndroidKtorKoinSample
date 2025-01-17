db0e5df0e8595a20545e0dcc2692a3fdb6f93fec
githubramesh <rameshkumar.rk1992@gmail.com>
2025-01-17 15:18:20 +0530

    - move movie endpoint to const variable

--- app/src/main/java/com/ktorkoin/sample/network/ApiService.kt
+++ app/src/main/java/com/ktorkoin/sample/network/ApiService.kt
@@ -6,9 +6,9 @@ import io.ktor.client.request.get
 class ApiService(private val client: HttpClient) {
 
     companion object {
-        private const val END_POINT =
-            "http://demo8912703.mockable.io/"
+        private const val END_POINT = "http://demo8912703.mockable.io/"
+        private const val GET_MOVIES_ENDPOINT = "movies"
     }
 
-    suspend fun getMovies() = client.get(END_POINT + "movies")
+    suspend fun getMovies() = client.get("$END_POINT$GET_MOVIES_ENDPOINT")
 }
